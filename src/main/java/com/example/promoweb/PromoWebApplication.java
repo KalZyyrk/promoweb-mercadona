50f995932b56d744d256b3e1f963a14d371da958
Ulysse <ulysse.hischke.dev@gmail.com>
2023-09-21 14:35:56 +0200

    init: first commit and init the sping project

---
+++ src/main/java/com/example/promoweb/PromoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.promoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromoWebApplication.class, args);
+    }
+
+}
